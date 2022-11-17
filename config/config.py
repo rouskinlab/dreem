aca8249fd54533628320f61c5b8645560091ad2b
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2022-11-17 10:57:11 -0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -12,24 +12,20 @@ def output_folder():
 def temp_folder():
   return 'temp_folder' #TODO
 
-def get_instructions():
-  return yaml.safe_load(open('config/config.yml'))
-
-def get_config():
-  instructions = get_instructions()
+def get_config(arguments):
   return {
     'input_folder': input_folder(),
     'input_files': input_files(),
     'output_folder': output_folder(),
     'temp_folder': temp_folder(),
     'demultiplexing':{
-      'use': instructions['demultiplexing']['use'],
+      'use': arguments['demultiplexing']['use'],
       'input': input_folder(),
       'temp': temp_folder()+'/demultiplexing',
       'output': output_folder()+'/demultiplexing'
     },
     'alignment':{
-      'input': output_folder()+'/demultiplexing' if instructions['demultiplexing']['use'] else input_folder(),
+      'input': output_folder()+'/demultiplexing' if arguments['demultiplexing']['use'] else input_folder(),
       'temp': temp_folder()+'/alignment',
       'output': output_folder()+'/alignment'
     },
@@ -39,18 +35,18 @@ def get_config():
       'output': output_folder()+'/vectoring'
     },
     'clustering':{
-      'use': instructions['clustering']['use'],
+      'use': arguments['clustering']['use'],
       'input': output_folder()+'/vectoring',
       'temp': temp_folder()+'/clustering',
       'output': output_folder()+'/clustering'
     },
     'aggregate':{
-      'input': output_folder()+'/clustering' if instructions['clustering']['use'] else output_folder()+'/vectoring',
+      'input': output_folder()+'/clustering' if arguments['clustering']['use'] else output_folder()+'/vectoring',
       'temp': temp_folder()+'/aggregate',
       'output': output_folder()+'/aggregate'
     },
     'post_processing':{
-      'use': instructions['post_processing']['use'],
+      'use': arguments['post_processing']['use'],
       'input': output_folder()+'/aggregate',
       'temp': temp_folder()+'/post_processing',
       'output': output_folder()+'/post_processing'
