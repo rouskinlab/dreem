03a313905f5ddfcc1407348bd6ab8d1821c7e00f
Alberic de Lajarte <42264772+AlbericDeLajarte@users.noreply.github.com>
2022-12-03 17:40:29 -0500

    Update run.py

--- dreem/alignment/run.py
+++ dreem/alignment/run.py
@@ -5,6 +5,7 @@ from dreem.alignment.alignment import align_reads
 
 @click.command()
 @click.option('--out_dir', '-o', default=os.getcwd(), type=click.Path(exists=True), help='Where to output files')
+@click.option('--sample', type=click.Path(exists=True), help='Name of the sequence alignment map file(s) folder')
 @click.option('--fasta', '-fa', type=click.Path(exists=True), help='Path to the fasta file', required=True)
 @click.option('--fastq1', '-fq1', help='Paths to the fastq1 file (forward primer). Enter multiple times for multiple files', type=click.Path(exists=True), required=True)
 @click.option('--fastq2', '-fq2', help='Paths to the fastq2 file (reverse primer). Enter multiple times for multiple files', type=click.Path(exists=True))
