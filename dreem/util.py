a67bf7df78b4f74d914814f3024e66ec462e2073
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2022-12-04 00:03:19 -0500

    test demultiplexing "file exists" exists

--- dreem/util.py
+++ dreem/util.py
@@ -1,4 +1,5 @@
 import yaml, sys, os, random
+import pandas as pd
 
 def make_folder(folder):
     if not os.path.exists(folder):
@@ -95,11 +96,15 @@ def create_sequence(length, bases=['A','T','C','G']):
 def print_fasta_line(f, id, seq):
     f.write('>{}\n{}\n'.format(id, seq))
 
-def write_fastq_pair(filename, number_of_reads, mutations, sequences, insertions, deletions, barcode_start, barcodes, constructs):
+def print_fastq_line(f, id, seq, qual):
+    f.write('@{}\n{}\n+\n{}\n'.format(id, seq, qual))    
+
+def generate_fastq_files(fastq1_name, fastq2_name, number_of_reads, mutations, sequences, insertions, deletions, barcode_start, barcodes, constructs):
     """Write a fastq file with the given parameters
     
     Arguments:
-        filename {str} -- where to write the fastq files
+        fastq1_name {str} -- where to write the fastq file for read 1
+        fastq2_name {str} -- where to write the fastq file for read 2
         number_of_reads {list} -- number of reads to generate for each construct [list]
         mutations {list} -- number of mutations to introduce in each read for each construct [list(list)]
         sequences {list} -- sequence to use for each construct [list]
@@ -114,8 +119,23 @@ def write_fastq_pair(filename, number_of_reads, mutations, sequences, insertions
     assert len(insertions) == len(constructs)
     assert len(deletions) == len(constructs)
     assert len(sequences) == len(constructs)
+
+    with open(fastq1_name, 'w') as f1, open(fastq2_name, 'w') as f2:
+        # write placeholder reads
+        for idx, c in enumerate(constructs):
+            for i in range(number_of_reads[idx]):
+                sequence = sequences[idx]
+                sequence = sequence[:barcode_start] + barcodes[idx] + sequence[barcode_start+len(barcodes[idx]):]
+                for j in mutations[idx][i]:
+                    sequence = sequence[:j] + next_base(sequence[j]) + sequence[j+1:]
+                for j in insertions[idx][i]:
+                    sequence = sequence[:j] + create_sequence(1) + sequence[j:]
+                for j in deletions[idx][i]:
+                    sequence = sequence[:j] + sequence[j+1:]
+                print_fastq_line(f1, '{}:{}'.format(c, i), sequence, 'F'*len(sequence))
+                print_fastq_line(f2, '{}:{}'.format(c, i), invert_sequence(sequence), 'F'*len(sequence))
     
-def write_fasta(filename, sequences, constructs, barcodes, barcode_start):
+def generate_fasta_file(filename, sequences, constructs, barcodes, barcode_start):
     """Write a fasta file with the given parameters
     
     Arguments:
@@ -130,4 +150,34 @@ def write_fasta(filename, sequences, constructs, barcodes, barcode_start):
         for b, c, s in zip(barcodes, constructs, sequences):
             print_fasta_line(f, c, s[:barcode_start] + b + s[barcode_start+len(b):])
             
-                    
+def generate_library_file(filename, constructs, barcodes, barcode_start):
+    """Write a library file with the given parameters
+
+    Arguments:
+        filename {str} -- where to write the library file
+        constructs {list} -- list of construct names [list]
+        barcodes {list} -- list of barcodes to use for each construct [list]
+        barcode_start {int} -- where to start the barcode in the read [int]
+    """
+    assert len(barcodes) == len(constructs), 'Number of barcodes and constructs must be the same'
+    assert sum([len(b) == len(barcodes[0]) for b in barcodes]) == len(barcodes), 'All barcodes must be the same length'
+    df = pd.DataFrame({'construct':constructs, 'barcode':barcodes, 'barcode_start':barcode_start, 'barcode_end':barcode_start+len(barcodes[0])})
+    df.to_csv(filename, index=False)
+
+def generate_demultiplexed_fastq(folder, number_of_reads, mutations, sequences, insertions, deletions, barcode_start, barcodes, constructs):
+    """Demultiplex a fastq file based on a library file
+
+    Arguments:
+        folder {str} -- where to write the demultiplexed fastq files
+        number_of_reads {list} -- number of reads to generate for each construct [list]
+        mutations {list} -- number of mutations to introduce in each read for each construct [list(list)]
+        sequences {list} -- sequence to use for each construct [list]
+        insertions {list} -- number of insertions to introduce in each read for each construct [list(list)]
+        deletions {list} -- number of deletions to introduce in each read for each construct [list(list)]
+        barcode_start {int} -- where to start the barcode in the read [int]
+        barcodes {list} -- list of barcodes to use for each construct [list]
+        constructs {list} -- list of construct names [list]
+    """
+
+    for idx, c in enumerate(constructs):
+        generate_fastq_files(os.path.join(folder, '{}_R1.fastq'.format(c)),  os.path.join(folder, '{}_R2.fastq'.format(c)), [number_of_reads[idx]], [mutations[idx]], [sequences[idx]], [insertions[idx]], [deletions[idx]], barcode_start, [barcodes[idx]], [c])
--- test/test_1_demultiplexing.py
+++ test/test_1_demultiplexing.py
@@ -3,8 +3,9 @@ import os
 import dreem.util as util
 import pandas as pd
 
-path_input = os.path.join(os.getcwd(),'test','test_files')
-path_output = os.path.join(os.getcwd(),'test','output')
+path_input = os.path.join(os.getcwd(),'test','test_files','input','demultiplexing')
+path_predicted = os.path.join(os.getcwd(),'test','test_files','predicted_output','demultiplexing')
+path_output = os.path.join(os.getcwd(),'test','test_files')
 
 def test_written():
     assert 0, 'Test isn\t done yet'
@@ -12,14 +13,15 @@ def test_written():
 def test_make_output_folder():
     util.clear_folder(os.path.join(os.getcwd(),'test/output'))
 
-def test_files_names():
-    for sample in os.listdir(os.path.join(os.getcwd(),'test','test_files')):
+def test_files_exists():
+    for sample in os.listdir(path_input):
         cmd = 'dreem-demultiplexing '\
             +'—-fastq1 \'{}/{}_R1.fastq\' '.format(os.path.join(path_input,sample),sample)\
             +'--fastq2 \'{}/{}_R2.fastq\' '.format(os.path.join(path_input,sample),sample)\
             +'—-library \'{}/library.csv\' '.format(os.path.join(path_input,sample))\
             +'-o ' + path_output
         util.run_cmd(cmd)
-        predicted_files_list = os.listdir(os.path.join(os.path.join(path_input,sample),'demultiplexed_fastq'))
-        output_files_list  = os.listdir(os.path.join(os.path.join(path_output,sample),'demultiplexed_fastq'))
-        assert set(output_files_list) == set(predicted_files_list)
+        
+        assert os.path.exists(os.path.join(path_output,'demultiplexing',sample)), 'Output folder for sample {} doesn\'t exist'.format(sample) 
+        for demultiplexed_file in os.listdir(os.path.join(path_output,'output','demultiplexing',sample)):
+            assert os.path.isfile(os.path.join(path_predicted,sample,demultiplexed_file)), 'File {} is missing'.format(demultiplexed_file)
