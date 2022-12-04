4dd0040654c2744f07e3e151e032755e86370c0b
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2022-12-04 17:38:28 -0500

    push

--- dreem/util.py
+++ dreem/util.py
@@ -1,5 +1,6 @@
 import yaml, sys, os, random
 import pandas as pd
+import subprocess
 
 def make_folder(folder):
     if not os.path.exists(folder):
@@ -181,3 +182,110 @@ def generate_demultiplexed_fastq(folder, number_of_reads, mutations, sequences, 
 
     for idx, c in enumerate(constructs):
         generate_fastq_files(os.path.join(folder, '{}_R1.fastq'.format(c)),  os.path.join(folder, '{}_R2.fastq'.format(c)), [number_of_reads[idx]], [mutations[idx]], [sequences[idx]], [insertions[idx]], [deletions[idx]], barcode_start, [barcodes[idx]], [c])
+
+
+def print_sam_header(f, construct, len_sequence):
+    f.write('@HD VN:1.0 SO:unsorted\n' + '@SQ SN:{} LN:{}\n'.format(construct, len_sequence))
+
+def print_sam_lines(f, read_name, sequence, construct, cigar):
+    f.write('{}\t3\t{}\t{}\t{}\t{}\t*\t0\t0\t{}\t{}\tNM:i:0\tAS:i:{}\tXS:i:0\n'.format(read_name, construct, 1, 255, cigar, len(sequence), sequence))
+    f.write('{}\t19\t{}\t{}\t{}\t{}\t*\t0\t0\t{}\t{}\tNM:i:0\tAS:i:{}\tXS:i:0\n'.format(read_name, construct, 1, 255, cigar, len(sequence), invert_sequence(sequence)))
+
+def make_cigar(len_sequence, mutations, insertions, deletions):
+    """Create a cigar string for a read with the given mutations and indels
+    The sequence is assumed to be 1-based.
+    The cigar string is 1-based.
+    The mutations and indels are 0-based.
+
+    The mutations and indels are assumed to be sorted. This is checked at the beginning of the code.
+
+    Arguments:
+        len_sequence {int} -- length of the sequence [int]
+        mutations {list} -- list of mutations [list]
+        insertions {list} -- list of insertions [list]
+        deletions {list} -- list of deletions [list]
+    """
+
+    # check that the mutations and indels are sorted
+    assert sorted(mutations) == mutations
+    assert sorted(insertions) == insertions
+    assert sorted(deletions) == deletions
+
+    for i in range(len(mutations)-1):
+        assert mutations[i]+1 != mutations[i+1], 'Consecutive mutations are not allowed'
+    
+    for i in range(len(insertions)-1):
+        assert insertions[i]+1 != insertions[i+1], 'Consecutive insertions are not allowed'
+    
+    for i in range(len(deletions)-1):
+        assert deletions[i]+1 != deletions[i+1], 'Consecutive deletions are not allowed'
+
+    # initialize the cigar string
+    cigar = ''
+
+    # keep track of the current position in the sequence
+    current_position = 1
+
+    # keep track of the current position in the cigar string
+    current_cigar_position = 1
+    
+    # loop through the mutations, insertions, and deletions to create the cigar string. If there's no mutation, insertion, or deletion at a position, then the position is a match.
+    while current_position <= len_sequence:
+        # if there's a mutation at the current position, then add a mutation to the cigar string
+        if current_position-1 in mutations:
+            cigar += '{}M1X'.format(current_cigar_position)
+            current_cigar_position = 0
+            current_position += 1
+        # if there's an insertion at the current position, then add an insertion to the cigar string
+        elif current_position-1 in insertions:
+            cigar += '{}M1I'.format(current_cigar_position)
+            current_cigar_position = 0
+            current_position += 1
+        # if there's a deletion at the current position, then add a deletion to the cigar string
+        elif current_position-1 in deletions:
+            cigar += '{}M1D'.format(current_cigar_position)
+            current_cigar_position = 0
+            current_position += 1
+        # if there's no mutation, insertion, or deletion at the current position, then add a match to the cigar string
+        else:
+            current_cigar_position += 1
+            current_position += 1
+    
+    # add the last match to the cigar string
+    cigar += '{}M'.format(current_cigar_position)
+    
+        
+            
+
+
+def generate_sam_files(folder, temp_folder, number_of_reads, mutations, sequences, insertions, deletions, barcode_start, barcodes, constructs):
+    """Generate sam files for the given parameters. 
+    The sam files are written from the input parameters, not from an external software. They do not use fastq files.
+
+    Arguments:
+        folder {str} -- where to write the sam files
+        number_of_reads {list} -- number of reads to generate for each construct [list]
+        mutations {list} -- number of mutations to introduce in each read for each construct [list(list)]
+        sequences {list} -- sequence to use for each construct [list]
+        insertions {list} -- number of insertions to introduce in each read for each construct [list(list)]
+        deletions {list} -- number of deletions to introduce in each read for each construct [list(list)]
+        barcode_start {int} -- where to start the barcode in the read [int]
+        barcodes {list} -- list of barcodes to use for each construct [list]
+        constructs {list} -- list of construct names [list]
+    """
+    assert len(barcodes) == len(constructs)
+    for idx, c in enumerate(constructs):
+        with open(os.path.join(folder, '{}.sam'.format(c)), 'w') as f:
+            print_sam_header(f, c, len(sequences[idx]))
+            for i in range(number_of_reads[idx]):
+                sequence = sequences[idx]
+                sequence = sequence[:barcode_start] + barcodes[idx] + sequence[barcode_start+len(barcodes[idx]):]
+                for j in mutations[idx][i]:
+                    sequence = sequence[:j] + next_base(sequence[j]) + sequence[j+1:]
+                for j in insertions[idx][i]:
+                    sequence = sequence[:j] + create_sequence(1) + sequence[j:]
+                for j in deletions[idx][i]:
+                    sequence = sequence[:j] + sequence[j+1:]
+                cigar = make_cigar(len(sequence), mutations[idx][i], insertions[idx][i], deletions[idx][i])
+                print_sam_lines(f, '{}:{}'.format(c, str(i).zfill(len(str(max(number_of_reads))))), sequence, 'F'*len(sequence), c, 1, '{}M'.format(len(sequence)), 0)
+
