00f2e98c718255109cf8178329248fcff0a7a23a
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2022-12-06 11:55:26 -0500

    tests are py and not notebooks anmore

--- test/generate_all_files_test.py
+++ test/generate_all_files_test.py
@@ -10,30 +10,37 @@ import pytest
 def run_notebook(notebook):
     current_dir = os.path.dirname(os.path.realpath(__file__))
     try: 
-        os.system('jupyter nbconvert --to script --execute '.format(notebook) + os.path.join(current_dir,'test_files_generators', notebook))
+        os.system('jupyter nbconvert --to notebook --execute '.format(notebook) + os.path.join(current_dir,'test_files_generators', notebook))
     except:
         raise 'Error running notebook {}'.format(notebook)
     try:
         os.system('rm -r {}'.format(os.path.join(current_dir,'test_files_generators','__pycache__')))
-        os.system('rm -r {}'.format(os.path.join(current_dir,'test_files_generators','*.py')))
+        #os.system('rm -r {}'.format(os.path.join(current_dir,'test_files_generators','*.py')))
         pass
     except:
         raise 'Error while removing nbconvert file. Issue is probably that the notebook hasn\'t been run properly'
+    
+def run_script(script):
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    try: 
+        os.system('python3 {}'.format(os.path.join(current_dir,'test_files_generators', script)))
+    except:
+        raise 'Error running script {}'.format(script)
 
 def test_make_demultiplexing_files():
-    run_notebook('make_demultiplexing_files.ipynb')
+    run_script('make_demultiplexing_files.py')
 
 def test_make_alignment_files():
-    run_notebook('make_alignment_files.ipynb')
+    run_script('make_alignment_files.py')
 
 def test_make_vectoring_files():
-    run_notebook('make_vectoring_files.ipynb')
+    run_script('make_vectoring_files.py')
 
 def test_make_clustering_files():
-    run_notebook('make_clustering_files.ipynb')
+    run_script('make_clustering_files.py')
 
 def test_make_aggregate_files():
-    run_notebook('make_aggregate_files.ipynb')
+    run_script('make_aggregate_files.py')
 
 # remove this test if you want to run the tests!!
 def test_make_predicted_output_an_output():
