fd15d07b0e62f3eea0e1c6790bacf7913b8f6860
Matty Allan <mfa@firebolt.harvardsecure.wireless.med.harvard.edu>
2022-12-07 19:33:40 -0500

    try - finally for temp fasta

--- dreem/align/main.py
+++ dreem/align/main.py
@@ -4,7 +4,7 @@ from typing import Optional, List
 from multiprocessing import Pool
 
 import dreem
-from dreem.util.util import FastaParser, FastaWriter, DEFAULT_PROCESSES, name_temp_file
+from dreem.util.util import FastaParser, FastaWriter, DEFAULT_PROCESSES, name_temp_file, try_remove
 from align import FastqInterleaver, FastqTrimmer, FastqMasker, FastqAligner, AlignmentCleaner, AlignmentFinisher
 
 def align_demultiplexed(ref, seq, fastq1, fastq2, output_folder, temp_folder):
@@ -31,8 +31,11 @@ def align_demultiplexed(ref, seq, fastq1, fastq2, output_folder, temp_folder):
 
     # Write a temporary FASTA file for the reference.
     temp_fasta = name_temp_file(temp_folder, ref, ".fasta")
-    FastaWriter(temp_fasta, {ref: seq})
-    
+    try:
+        FastaWriter(temp_fasta, {ref: seq})
+    finally:
+        try_remove(temp_fasta)
+
 
         
 
