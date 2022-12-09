bec60ae64d3decd4aa2a93fb46b5d5fd4649784d
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2022-12-08 19:27:14 -0500

    rebase CLI

--- dreem/vector/cli.py
+++ dreem/vector/cli.py
@@ -1,26 +1,23 @@
-import dreem
 import click
-import dreem.util as util
-import os, sys
 from main import run
+from click_option_group import optgroup
+from dreem.util.cli_args import *
 
 @click.command()
-@click.option('--out_dir', '-o', default=os.getcwd(), type=click.Path(exists=True), help='Where to output files')
-@click.option('--fasta', '-fa', type=click.Path(exists=True), help='Path to the fasta file', required=True)
-@click.option('--input_dir', '-id', help='Paths to the directory(ies) of bam files', type=click.Path(exists=True), required=True, multiple=True)
-@click.option("-P", "--parallel",
-              type=click.Choice(["profiles", "reads", "off", "auto"],
-                                case_sensitive=False),
-              default="auto",
-              help="Parallelize the processing of mutational PROFILES or "
-              "READS within each profile, turn parallelization OFF, or AUTO"
-              "matically choose the parallelization method (default: auto).")
-@click.option("-c", "--coords", type=(str, int, int), multiple=True, help="coordinates for reference: '-c ref-name first last'")
-@click.option("-p", "--primers", type=(str, util.Primer, util.Primer), multiple=True, help="primers for reference: '-p ref-name fwd rev'")
-@click.option("--fill/--no-fill", default=False,
-              help="Fill in coordinates of reference sequences for which "
-                   "neither coordinates nor primers were given (default: no).")
 
+@optgroup.group('I/O')
+@fasta
+@input_dir
+@out_dir
+@library
+
+@optgroup.group('Selection')
+@coords
+@primers
+@fill
+
+@optgroup.group('Miscellaneous')
+@verbose
 
 def cli(**args):
     run(**args)
