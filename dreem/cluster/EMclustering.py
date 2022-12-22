a2f444721fd7089fbd7891daee674780800e9386
AlbericDeLajarte <albericlajarte@gmail.com>
2022-12-22 17:37:47 -0500

    removed old test code

--- dreem/cluster/EMclustering.py
+++ dreem/cluster/EMclustering.py
@@ -247,13 +247,13 @@ class EMclustering:
         # ------------------------ Iterations end ---------------------------- #
 
         # BIC = calc_BIC(N, D, self.K, log_like_list[-1]) ## !! Technically it should be the number of non G/T bases, not D !!
-        # return {'mu': final_mu, 'pi': final_real_pi, 'log_likelihood': log_like_list[-1]}
-        return {'mu': final_mu, 'pi': final_real_pi, 'log_likelihood': log_like_list[-1], "dT": np.mean(dt)} # !! For testing !!
+        return {'mu': final_mu, 'pi': final_real_pi, 'log_likelihood': log_like_list[-1]}
+        # return {'mu': final_mu, 'pi': final_real_pi, 'log_likelihood': log_like_list[-1], "dT": np.mean(dt)} # !! For testing !!
 
 
 ## ----- Testing if the above code gives same results as original code ----- ##
 
-if __name__ == '__main__':
+if __name__ == '__main__' and False:
 
     exp_path = "/Users/Alberic/Desktop/Pro/RouskinLab/projects/DREEM/"
     bit_Vector_total = np.load(exp_path+"bit_vector.npy")
