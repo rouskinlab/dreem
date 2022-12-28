51705b97280af867a268f80d03cdf3b07bc8309f
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2022-12-28 13:30:24 -0800

    fix bug vectoring

--- dreem/vector/mprofile.py
+++ dreem/vector/mprofile.py
@@ -293,15 +293,11 @@ class MutationalProfile(Region):
     def short_path(self):
         return os.path.join(self.sample_name,
                             self.ref_name.decode(),
-<<<<<<< HEAD
-                            f"{self.first}-{self.last}")
+                            f"{self.first}_{self.last}")
     
     def __str__(self) -> str:
         return (f"Mutational Profile of sample '{self.sample_name}' reference"
-                f" '{self.ref_name}' region {self.first}-{self.last}")
-=======
-                            f"{self.first}_{self.last}")
->>>>>>> 8f8b847 (fix vectoring path to output)
+                f" '{self.ref_name}' region {self.first}_{self.last}")
 
 
 class VectorIO(MutationalProfile):
