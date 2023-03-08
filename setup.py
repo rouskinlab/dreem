b8be16bbe8c2da9d46edef4989479147aec24c01
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2023-03-07 17:23:30 -0800

    build works

--- setup.py
+++ setup.py
@@ -1,30 +1,23 @@
 from setuptools import setup, find_packages
-from dreem import __version__
+#from dreem import __version__
 import sys
 
+print("Python version: {}".format(sys.version))
 requirements = []
 with open('requirements.txt', 'r') as fh:
     for line in fh:
         requirements.append(line.strip())
 
 
-PYTHON_VERSION = (3,10)
+#PYTHON_VERSION = (3,10)
 
-if sys.version_info < PYTHON_VERSION:
-    sys.exit(f"Python >= {PYTHON_VERSION[0]}.{PYTHON_VERSION[1]} required.")
+#if sys.version_info < PYTHON_VERSION:
+#    sys.exit(f"Python >= {PYTHON_VERSION[0]}.{PYTHON_VERSION[1]} required.")
 
 readme = open('README.md').read()
 
 setup(
-   name='dreem',
-   version=__version__,
-   license="MIT",
-   description=("Implementation of Prof Silvi Rouskin's DREEM algorithm "
-                "by Yves Martin, Scott Grote, and Matty Allan"),
-   author="Yves Martin des Taillades, Scott Grote, and Matty Allan",
-   author_email='yves@martin.yt',
-   long_description=readme,
-   url='https://github.com/rouskinlab/dreem',
+  long_description=readme,
    packages=find_packages(),
    package_dir={'dreem': 'dreem'},
    py_modules=[
