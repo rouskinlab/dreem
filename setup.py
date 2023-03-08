c14aa7874a3c9f89a5360128d834f4396e51bea9
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2023-03-08 12:38:36 -0800

    version pushed to pypi 0.1.1

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 #from dreem import __version__
-import sys
+import sys, os
 
+requirements_path = os.path.join(os.getcwd(), 'requirements.txt')
+requirements_path = '/Users/ymdt/src/dreem/requirements.txt'
 print("Python version: {}".format(sys.version))
 requirements = []
-with open('requirements.txt', 'r') as fh:
+with open(requirements_path, 'r') as fh:
     for line in fh:
         requirements.append(line.strip())
 
@@ -17,6 +19,7 @@ with open('requirements.txt', 'r') as fh:
 readme = open('README.md').read()
 
 setup(
+    version='0.1.0',
   long_description=readme,
    packages=find_packages(),
    package_dir={'dreem': 'dreem'},
