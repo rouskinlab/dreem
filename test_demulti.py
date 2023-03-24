f222a9f31764b965fef938e87c5e16956525ded9
heWhosShouldersBlockTheSun <scott_grote@hms.harvard.edu>
2023-03-24 16:03:18 -0400

    changed test demulti for O2

--- test_demulti.py
+++ test_demulti.py
@@ -1,23 +1,24 @@
 
 import os
 import sys
- 
+
 #sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import os
 from dreem.main import run
 
 
 if __name__ == '__main__':
-    print("/Users/scottgrote/Documents/absolutely_final_repo/dreem_data/short_R1.fastq".split("/")[-1].split("_R")[0])
-    """run(
-        fasta="/Users/scottgrote/Documents/absolutely_final_repo/dreem_data/new_ref.fasta",
-        fastq1="/Users/scottgrote/Documents/absolutely_final_repo/dreem_data/short_R1.fastq",
-        fastq2="/Users/scottgrote/Documents/absolutely_final_repo/dreem_data/short_R2.fastq",
-        library="/Users/scottgrote/Documents/absolutely_final_repo/dreem_data/updated_library.csv",
+
+    run(
+        fasta="/n/data1/hms/microbiology/rouskin/lab/projects/TheJuicyOrange/final_dreem_repo/test_files/all_ref.fasta",
+        fastq1=sys.argv[1],
+        fastq2=sys.argv[2],
+        library="/n/data1/hms/microbiology/rouskin/lab/projects/TheJuicyOrange/final_dreem_repo/test_files/library_full.csv",
         clipped=2,
         mismatch_tolerence=2,
         index_tolerance=20,
         parallel_demultiplexing=True,
         demult_on=True,
         demulti_overwrite=False,
-        )"""
+	out_dir="/n/data1/hms/microbiology/rouskin/lab/projects/TheJuicyOrange/final_dreem_repo/test_files/"
+        )
