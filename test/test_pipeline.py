dd918a2003d1bb17f07e06ac01f697095d2a5f13
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2023-04-02 23:19:30 -0700

    add test files

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -8,8 +8,8 @@ import json
 import shutil
 
 # To remove
-if os.path.exists(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'RNAstructure')):
-    sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'RNAstructure/exe'))
+if os.path.exists(os.path.join(os.path.dirname(__file__), 'RNAstructure')):
+    sys.path.append(os.path.join(os.path.dirname(__file__)), 'RNAstructure/exe')
 
 dreem_root = os.path.dirname(os.path.dirname(__file__))
 test_files = os.path.join(dreem_root, 'test_files')
